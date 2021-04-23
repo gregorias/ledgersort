a5d202f8ccfbcd9bb2762e8d194b8bca4c71e59b
Grzegorz Milka <grzegorzmilka@gmail.com>
2021-04-23 11:14:31 +0200

    Update project settings

--- src/Types.hs
+++ src/Types.hs
@@ -1,20 +1,19 @@
-module Types
-  ( DatedChunk (..),
-    Journal (..),
-  )
-where
+module Types (
+  DatedChunk (..),
+  Journal (..),
+) where
 
 import Data.Text (Text)
 import Data.Time (Day)
 
 data DatedChunk = DatedChunk
-  { dcDate :: Day,
-    dcContent :: Text
+  { dcDate :: Day
+  , dcContent :: Text
   }
-  deriving (Eq, Show)
+  deriving stock (Eq, Show)
 
 data Journal = Journal
-  { jPreamble :: Text,
-    jDatedChunks :: [DatedChunk]
+  { jPreamble :: Text
+  , jDatedChunks :: [DatedChunk]
   }
-  deriving (Eq, Show)
+  deriving stock (Eq, Show)
